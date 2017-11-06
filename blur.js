dc5825cc66a03085e97f13a6c5ea3d4caa886c2d
TheoRice <theo.rice1996@gmail.com>
2017-11-05 20:20:08 -0500

    first commit. Blurs episodes on imdb load, and unblurs/reblurs with click of button

---
+++ blur.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', buttonControl, false);
+
+function buttonControl() {
+
+    document.getElementById("unblur").addEventListener('click', () => {
+        function unblur() {
+            var credit = document.getElementsByClassName("credit");
+            for (i = 0; i < credit.length; i++) {
+                credit[i].style.color = "";
+                credit[i].style.textShadow = "";
+            }
+            var episodes = document.getElementsByClassName("toggle-episodes");
+            for (i = 0; i < episodes.length; i++) {
+                episodes[i].style.color = "";
+                episodes[i].style.textShadow = "";
+            }
+        }
+
+        chrome.tabs.executeScript({
+            code: '(' + unblur + ')();'
+        })
+    });
+
+    document.getElementById("reblur").addEventListener('click', () => {
+        function reblur() {
+            var epCredits = document.getElementsByClassName("credit");
+            for (i = 0; i < epCredits.length; i++) {
+                epCredits[i].style.color = "transparent";
+                epCredits[i].style.textShadow = "0 0 5px rgba(0,0,0,0.5)";
+            }
+            var episodes = document.getElementsByClassName("toggle-episodes");
+            for (i = 0; i < episodes.length; i++) {
+                episodes[i].style.color = "transparent";
+                episodes[i].style.textShadow = "0 0 5px rgba(0,0,0,0.5)";
+            }
+        }
+
+        chrome.tabs.executeScript({
+            code: '(' + reblur + ')();'
+        })
+    });
+}
